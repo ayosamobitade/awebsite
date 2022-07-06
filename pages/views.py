400d57ca8be5b17b76060ce6e8d21ecc2e5be745
UncleAyo <ayosamobitade@gmail.com>
2022-07-06 22:31:40 +0100

    some update

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Page
 
 def index(request, pagename):
     pagename = '/' + pagename
-    pg = Page.objects.get(permalink=pagename)
+    pg = get_object_or_404(Page, permalink=pagename)
     context = {
         'title': pg.title,
         'content': pg.bodytext,
         'last_updated': pg.update_date,
+        'page_list':Page.objects.all()
     }
     return render(request, 'pages/page.html', context)

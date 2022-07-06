16a950f31e900225856c66ca2a3b564660dde3d9
UncleAyo <ayosamobitade@gmail.com>
2022-07-06 21:44:04 +0100

    some update

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, {'pagename':''}, name = 'home'),
-    path('<str:pagename>', views.index, name='index')
+    path('', views.index, {'pagename': ''}, name = 'home'),
+    path('<str:pagename>', views.index, name='index'),
 ]

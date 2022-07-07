811431cb2a934e632837113d5ab67f8ba92ed150
UncleAyo <ayosamobitade@gmail.com>
2022-07-07 16:38:05 +0100

    some update

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+STATUS_CHOICE = (
+('NEW', 'New Site'),
+('Ex', 'Existing Site'),
+)
+
+PRIORITY_CHOICES = (
+    ('U', 'Urgent - 1 week or less'),
+    ('N', 'Normal - 2 to 4 weeks'),
+    ('L', 'Low - Still Researching'),
+)
+
+class Quote(models.Model):
+    name = models.CharField(max_length = 100)
+    position = models.CharField(max_length = 60, blank = True)
+    company = models.CharField(max_length = 60, blank = True)
+    address =  models.CharField(max_length = 200, blank = True)
+    phone = models.CharField(max_length = 200, blank = True)
+    email = models.EmailField()
+    web = models.URLField(blank = True)
+    

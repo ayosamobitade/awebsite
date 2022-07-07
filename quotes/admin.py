e07ed10c983af0a7a2ef42fd39eca411de1166b2
UncleAyo <ayosamobitade@gmail.com>
2022-07-07 17:26:46 +0100

    some update

--- quotes/admin.py
+++ quotes/admin.py
@@ -3,3 +3,28 @@ from django.contrib import admin
 # Register your models here.
 from .models import Quote
 
+admin.site.register(Quote)
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'company', 'submitted', 'quotedate', 'quoteprice')
+    list_filter = ('submitted', 'quotedate')
+    readonly_fields = ('submitted',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'email', 'descriprion')
+        }),
+        ('Contact Information', {
+            'classes': ('postion', 'company', 'address', 'phone', 'web')
+
+        }),
+        ('Job Information', {
+            'classes':('collapse',),
+            'fields':('sitestatus', 'priority', 'submitted')
+
+        }),
+        ('Quote Admin',{
+            'classes':('collapse',),
+            'fields':('quotedate', 'quoteprice', 'username')
+        }),
+    )

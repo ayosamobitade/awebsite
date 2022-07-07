719d4aa19bf076fb31119a85df9f575456076a6c
UncleAyo <ayosamobitade@gmail.com>
2022-07-07 14:39:51 +0100

    some update

--- awebsite/settings.py
+++ awebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a#4q3ll3hx0%61x@kwjc=137midnt1r!^-86p059&i$a@j-f$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

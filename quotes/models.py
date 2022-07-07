46f9f903e14a5877d583e8a62dcf92456f3f451c
UncleAyo <ayosamobitade@gmail.com>
2022-07-07 16:57:06 +0100

    some update

--- quotes/models.py
+++ quotes/models.py
@@ -19,9 +19,19 @@ class Quote(models.Model):
     position = models.CharField(max_length = 60, blank = True)
     company = models.CharField(max_length = 60, blank = True)
     address =  models.CharField(max_length = 200, blank = True)
-    phone = models.CharField(max_length = 200, blank = True)
+    phone = models.CharField(max_length = 30, blank = True)
     email = models.EmailField()
     web = models.URLField(blank = True)
     description = models.TextField()
-    sitestatus = models.CharField(max_length = 40, choices= STATUS_CHOICE)
-    
+    sitestatus = models.CharField(max_length = 20, choices= STATUS_CHOICE)
+    priority = models.CharField(max_length=40, choices = PRIORITY_CHOICES)
+    jobfile = models.FileField(upload_to = 'uploads/', blank = True)
+    submitted  = models.DateField(auto_now_add = True)
+    quotedate = models.DateField(blank=True, null = True)
+    quoteprice = models.DecimalField(decimal_places=2, max_digits=7, blank=True, default = 0)
+    username = models.ForeignKey(User, blank=True, null = True, on_delete= models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.id)
+

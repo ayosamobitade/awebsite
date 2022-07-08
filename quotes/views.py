d9966a8d095a8e6e8da9d6dde32bfe4be5308075
UncleAyo <ayosamobitade@gmail.com>
2022-07-08 13:59:38 +0100

    some update

--- quotes/views.py
+++ quotes/views.py
@@ -5,3 +5,22 @@ from django.http import HttpResponseRedirect
 
 from .models import Quote
 from .forms import QuoteForm
+
+from pages.models import Page
+
+def quote_req(request):
+    submitted = False
+    if request.method == 'POST'
+    form = QuoteForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/quote/?submitted=True')
+    else:
+        form = QuoteForm()
+        if 'submitted' in reqeust.GET:
+            submitted = True
+    
+    return render(request, 'quotes/quote.html',
+    {'form' form, 'page_list': Page.objects.all(), 'submitted':submitted})
+
+    

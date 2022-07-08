214a64ab6422375315116f205b4145bb0cbce495
UncleAyo <ayosamobitade@gmail.com>
2022-07-08 14:02:48 +0100

    some update

--- quotes/views.py
+++ quotes/views.py
@@ -10,17 +10,16 @@ from pages.models import Page
 
 def quote_req(request):
     submitted = False
-    if request.method == 'POST'
-    form = QuoteForm(request.POST, request.FILES)
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect('/quote/?submitted=True')
-    else:
-        form = QuoteForm()
-        if 'submitted' in reqeust.GET:
-            submitted = True
+    if request.method == 'POST':
+        form = QuoteForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/quote/?submitted=True')
+        else:
+            form = QuoteForm()
+            if 'submitted' in request.GET:
+                submitted = True
     
     return render(request, 'quotes/quote.html',
     {'form' form, 'page_list': Page.objects.all(), 'submitted':submitted})
 
-    

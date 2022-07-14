51a3cd8bac9e67b1e13d67070790522ed3f0cf30
UncleAyo <ayosamobitade@gmail.com>
2022-07-14 15:00:43 +0100

    some updates

--- awebsite/urls.py
+++ awebsite/urls.py
@@ -22,7 +22,10 @@ from quotes.views import Register
 urlpatterns = [
     path('testpage', TemplateView.as_view(template_name='pages/page.html')),
     path('admin/', admin.site.urls),
+    path('register/success/', TemplateView.as_view(template_name="registration/success.html"), name="register-success"),
+    path('register/', Register.as_view(), name ="register"),
     path('quote/', include('quotes.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('pages.urls')),
     
     

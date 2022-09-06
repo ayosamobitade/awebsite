0dd04fa69b442340d7c332da147c71a2979bff41
UncleAyo <ayosamobitade@gmail.com>
2022-09-06 16:13:58 +0100

    some updates

--- quotes/forms.py
+++ quotes/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from .models import Quote
 
 class QuoteForm(ModelForm):
-    requeired_css_class = 'required'
+    required_css_class = 'required'
     
     class Meta:
         model = Quote
